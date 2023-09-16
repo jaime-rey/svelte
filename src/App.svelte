54b9da81bb735d44583c6bcd1f22a83f1afc54a9
jaime-rey <jaimereycasado@gmail.com>
2023-09-17 00:28:41 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    let age = 30;
+    let name = "Dom"
+</script>
+
+<style>
+
+</style>
+
+<p>
+    Your name is {name} and your age is {age}
+</p>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    let age = 30;
+    let name = "Dom"
+    let greeting = "";
+    $: {
+        greeting = `Your name is ${name} and your age is ${age}`
+        console.log( `Your name is ${name} and your age is ${age}`)
+    }
+    function handleClick(){
+        ++age;
+    }
+</script>
+
+<style>
+
+</style>
+
+<p>
+    {greeting}
+</p>
+<button on:click={handleClick}>Increase Age</button>
